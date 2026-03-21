5b541c46855c6074a961520d7f1c0c2e58754980
Jiakai Bi <bi.jk@hotmail.com>
2026-03-21 16:48:57 +0800

    feat(downloader): 添加Fanbox和kemono.party图片下载脚本
    - 新增Fanbox图片下载器用户脚本，支持单张和打包下载
    - 实现下载按钮自动添加及备份检测机制
    - 支持自定义命名格式和自动下载设置
    - 提供下载设置面板，方便用户修改配置
    - 新增kemono.party图片下载器用户脚本，支持单张和ZIP下载
    - 增加重新尝试下载机制提高稳定性
    - 实现作者名获取及图片链接筛选功能
    - 添加“需要首图”复选框控制首张图片下载
    - 优化按钮插入逻辑与页面DOM变动监听

---
+++ kemono_party_downloader.js
@@ -0,0 +1,231 @@
+// ==UserScript==
+// @name         kemono.party图片下载器
+// @name:en      kemono.party Downloader
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Download kemono.party Images.
+// @description:en  Download kemono.party Images.
+// @author       endedge
+// @match        https://kemono.party/*
+// @match        https://kemono.su/*
+// @match        https://kemono.cr/*
+// @icon         https://kemono.party/favicon.ico
+// @grant        GM_xmlhttpRequest
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jszip/3.2.0/jszip.min.js
+// ==/UserScript==
+
+(function () {
+    var dlList = [];
+    var zip;
+    var totalImage = -1;
+    var addFile = (name, content) => zip.file(name, content);
+
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.id = "needFirstImageCheckbox";
+
+    var checkboxLabel = document.createElement('label')
+    checkboxLabel.htmlFor = "needFirstImageCheckbox";
+    checkboxLabel.appendChild(document.createTextNode('Need First Image'));
+
+    var span = document.createElement("span");
+    span.textContent = "Download Pictures";
+
+    var spanZip = document.createElement("span");
+    spanZip.textContent = "Download ZIP";
+
+    var modifiedPicName = document.createElement("modifiedNames");
+    modifiedPicName.textContent = "Modified Names";
+
+    var button = document.createElement("button");
+    button.classList.add("post__download");
+    button.type = "button";
+    button.appendChild(span);
+
+    button.onclick = function () {
+        var imgs = getImageLinks();
+        // console.log(imgs);
+        downloadImages(...imgs);
+    }
+
+    var buttonZip = document.createElement("button");
+    buttonZip.classList.add("zip__download");
+    buttonZip.type = "button";
+    buttonZip.appendChild(spanZip);
+
+    buttonZip.onclick = function () {
+        zip = new JSZip();
+        var imgs = getImageLinks();
+        // console.log(imgs);
+        downloadImages_ZIP(...imgs);
+    }
+
+    var buttonModifiedPicName = document.createElement("button");
+    buttonModifiedPicName.classList.add("modified_name");
+    buttonModifiedPicName.type = "button";
+    buttonModifiedPicName.appendChild(modifiedPicName);
+    buttonModifiedPicName.onclick = function () {
+        modifiedNames();
+    }
+
+    const p = document.getElementsByClassName("post__actions")[0];
+    if (p) {
+        observer.disconnect();
+        // 执行操作
+        p.appendChild(button);
+        p.appendChild(buttonZip);
+        p.appendChild(buttonModifiedPicName)
+        p.appendChild(checkbox);
+        p.appendChild(checkboxLabel);
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+    }
+
+    const observer = new MutationObserver((mutations) => {
+        const p = document.getElementsByClassName("post__actions")[0];
+        if (p) {
+            observer.disconnect();
+            // 执行操作
+            p.appendChild(button);
+            p.appendChild(buttonZip);
+            p.appendChild(buttonModifiedPicName)
+            p.appendChild(checkbox);
+            p.appendChild(checkboxLabel);
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true,
+            });
+        }
+    });
+
+    // 监听整个文档的变化
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
+
+    //var p = document.getElementsByClassName("post__actions")[0];
+    //p.appendChild(button);
+    //p.appendChild(buttonZip);
+    //p.appendChild(checkbox);
+    //p.appendChild(checkboxLabel);
+
+    function downloadImages(...urls) {
+        var name = document.getElementsByClassName("post__title")[0].getElementsByTagName("span")[0].textContent;
+        var index = 0;
+        urls.forEach(function (url) {
+            forceDownload(url, name + "_" + index + "." + url.split(".")[url.split(".").length - 1], false);
+            index++;
+        });
+        return undefined;
+    }
+
+    function downloadImages_ZIP(...urls) {
+        var index = 0;
+        var name = document.getElementsByClassName("post__title")[0].getElementsByTagName("span")[0].textContent;
+        totalImage = urls.length;
+        urls.forEach(function (url) {
+            forceDownload(url, name + "_" + index + "." + url.split(".")[url.split(".").length - 1], true);
+            index++;
+        });
+        return undefined;
+    }
+
+    function forceDownload(url, fileName, zipFlag) {
+        forceDownloadWithRetry(url, fileName, zipFlag, 0);
+    }
+
+    function forceDownloadWithRetry(url, fileName, zipFlag, count) {
+        if (dlList.includes(fileName)) {
+            return;
+        }
+        GM_xmlhttpRequest({
+            method: "GET",
+            url: url,
+            binary: true,
+            responseType: "blob",
+            onload: function (response) {
+                console.log("[Downloader.js] Downloaded " + fileName);
+                if (response.response.size / 1024 < 0) {
+                    console.error("[Downloader.js] Failed downloading file " + fileName);
+                    count++;
+                    console.log("[Downloader.js] " + fileName + " retry count: " + count);
+                    forceDownloadWithRetry(url, fileName, zipFlag, count);
+                    return;
+                }
+                dlList.push(fileName);
+                var urlCreator = window.URL || window.webkitURL;
+                var imageUrl = urlCreator.createObjectURL(response.response);
+                if (!zipFlag) {
+                    var tag = document.createElement('a');
+                    tag.href = imageUrl;
+                    tag.download = fileName;
+                    document.body.appendChild(tag);
+                    tag.click();
+                    document.body.removeChild(tag);
+                    return;
+                }
+                addFile(fileName, response.response);
+                if (dlList.length === totalImage) {
+                    console.info("[Downloader.js] Preparing zip file...");
+                    zip.generateAsync({ type: 'blob' }).then(function (blob) {
+                        var imageUrl = urlCreator.createObjectURL(blob);
+                        var tag = document.createElement('a');
+                        tag.href = imageUrl;
+                        tag.download = "[" + generateAuthorName() + "] " + document.getElementsByClassName("post__title")[0].getElementsByTagName("span")[0].textContent + ".zip";
+                        document.body.appendChild(tag);
+                        tag.click();
+                        document.body.removeChild(tag);
+                    });
+                }
+            },
+            onprogress: function (e) {
+                if (e.callengthComputable) {
+                    var ratio = Math.floor((e.loaded / e.total) * 100) + '%';
+                    console.log("[Downloader.js] " + fileName + " > " + ratio);
+                    return;
+                }
+                console.log("[Downloader.js] " + fileName + " downloaded " + (e.loaded / 1024).toFixed(3) + "kB (No total length found)");
+            },
+            onerror: function (e) {
+                console.error("[Downloader.js] Failed downloading file " + fileName);
+                count++;
+                console.log("[Downloader.js] " + fileName + " retry count: " + count);
+                forceDownloadWithRetry(url, fileName, zipFlag, count);
+            },
+        });
+    }
+
+    function generateAuthorName() {
+        var str = document.getElementsByClassName("post__user-name")[0].textContent;
+        return str.replace(/\s+/g, "").replace(/[\r\n]/g, "");
+    }
+
+    function getImageLinks() {
+        var thumbnail_list = document.getElementsByClassName("fileThumb image-link");
+        var imgs = [];
+        for (let index = 0; index < thumbnail_list.length; index++) {
+            if (index === 0 && !document.getElementById("needFirstImageCheckbox").checked) {
+                continue;
+            }
+            imgs.push(thumbnail_list[index]["href"]);
+        }
+        return imgs;
+    }
+
+    function modifiedNames() {
+        var thumbnail_list = document.getElementsByClassName("fileThumb image-link");
+        var name = document.getElementsByClassName("post__title")[0].getElementsByTagName("span")[0].textContent;
+        for (let index = 0; index < thumbnail_list.length; index++) {
+            if (index === 0 && !document.getElementById("needFirstImageCheckbox").checked) {
+                continue;
+            }
+            var url = thumbnail_list[index]["href"];
+            thumbnail_list[index]["download"] = name + "_" + index + "." + url.split(".")[url.split(".").length - 1];
+        }
+    }
+})();
